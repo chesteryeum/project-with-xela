21d60afd6f01d154a7b9d4ceb41fc14be672cd85
Alex Bardales <xela144@users.noreply.github.com>
2022-04-23 12:35:03 -0700

    Issue/subtract (#6)
    * Added "add" endpoint
    
    * Added a "subtract" endpoitn as per @chesteryeum

--- src/main.py
+++ src/main.py
@@ -10,3 +10,7 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/subtract/")
+async def subtract_integers(a: int, b: int):
+    return a - b
